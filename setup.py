6f37a72aeef2eb262ff37d7bd7b118e877397947
Olivier WATTÉ <olivier@ipeos.com>
2017-05-23 10:21:59 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # @Date:   2017-04-26T04:39:06-04:00
 # @Email:  owatte@ipeos.com
 # @Last modified by:   user
-# @Last modified time: 2017-05-22T15:38:07-04:00
+# @Last modified time: 2017-05-23T10:21:26-04:00
 # @License: GPLv3
 # @Copyright: IPEOS I-Solutions
 
@@ -20,15 +20,17 @@ setup(
     version=rlieh_leds.__version__,
     packages=find_packages(),
     author="Olivier Watté - RLIEH project",
-    author_email="owatte@emnet.cc",
-    description="This module provides an interface to manage leds light-system",
+    author_email="owatte@lebiklab.com",
+    description="This module provides an interface to manage PWM on RLIEH systems",
     long_description=open('README.md').read(),
     include_package_data=True,
-    url='http://github.com/owatte/rlieh_leds',
+    url='http://github.com/owatte/rlieh-pwm',
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 3 - Alpha",
+        "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
