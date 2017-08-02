1697ab5de6640bfbef8ca538f6c2761736d21b49
Olivier WATTÉ <olivier@ipeos.com>
2017-08-02 08:15:34 -0400

    add some test and GPLv3 headers

--- rlieh_pwm/tests.py
+++ rlieh_pwm/tests.py
@@ -2,27 +2,42 @@
 # @Date:   2017-07-25T12:22:59-04:00
 # @Email:  owatte@ipeos.com
 # @Last modified by:   user
-# @Last modified time: 2017-08-01T19:39:53-04:00
+# @Last modified time: 2017-08-02T08:08:45-04:00
 # @License: GPLv3
 # @Copyright: IPEOS I-Solutions
+
+# Rlieh-pwm provides an interface to manage PWM on RLIEH systems.
+# Copyright (C) 2017 Olivier Watte
+#
+# This file is part of rlieh-pwm.
+#
+# Rlieh-pwm is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Rlieh-pwm is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rlieh-pwm.  If not, see <http://www.gnu.org/licenses/>.
+
 
 from numpy import allclose
 import unittest
 from .core import RliehPWM
 
+
 class TestCalcPauseTime(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
 
     def test__calc_pause_time(self):
         mytest = RliehPWM()
-        actual = mytest._calc_pause_time(1000, 1000)
-        expected = 60
-        self.assertEqual(actual, expected, '60 should be the return value.')
+        actual = mytest._calc_pause_time(60, 500)
+        expected = 0.12
+        self.assertEqual(actual, expected)
+
 
 class TestCalcSteps(unittest.TestCase):
     '''Perfom test on RliehPWM._calc_steps().'''
@@ -56,6 +71,15 @@ class TestCalcSteps(unittest.TestCase):
         expected = [97.1, 97.2, 97.3, 97.4, 97.5, 97.6, 97.7, 97.8, 97.9, 98.0,
                     98.1, 98.2, 98.3, 98.4, 98.5, 98.6, 98.7, 98.8]
         self.assertTrue(allclose(actual, expected))
+
+
+class TestConvertPercentToBlaster(unittest.TestCase):
+    def test___convert_percent_to_blaster__valueerror(self):
+        mytest = RliehPWM()
+        for percent in [-0.001, -1, 101, 100.1]:
+            self.assertRaises(ValueError,
+                              mytest._convert_percent_to_blaster,
+                              percent)
 
 
 if __name__ == "__main__":
